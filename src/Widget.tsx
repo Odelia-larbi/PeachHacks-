ebe480ab2aad1fff2d0a2e28bda5574869b3ebfe
Odelia-larbi <145396652+Odelia-larbi@users.noreply.github.com>
2025-04-10 16:59:02 -0400

    widget customizations

---
+++ src/Widget.tsx
@@ -0,0 +1,15 @@
+import './Widget.css';
+
+const Widget = () => {
+  return (
+    <div className="widget">
+      <p>Welcome to widgets</p>
+      <h2>Weather</h2>
+      <p>72°F and sunny 🌞</p>
+    </div>
+  );
+};
+
+export default Widget;
+
+  
